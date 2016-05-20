aae36645d055dabaeb6c2fc6ce594db472840c2c
roszczypala <roszczi@gmail.com>
2016-05-20 12:06:24 +0200

    1 zadanie

---
+++ src/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "coderslab";
+$baseName = "twitter";
+
+$conn = new mysqli($servername, $username, $password, $baseName);
+
+if($conn->connect_error) {
+	die("Połączenie nieudane. Błąd: ".$conn->connect_error);
+} else {
+        $conn->set_charset("utf8");
+}
+
